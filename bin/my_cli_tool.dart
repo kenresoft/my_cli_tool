b1386c76b0da86e98e8d4777fd5d5ffc57ff4cdc
kenresoft <amadikenneth8@gmail.com>
2024-06-07 13:07:43 +0100

    Initial commit

---
+++ bin/my_cli_tool.dart
@@ -0,0 +1,5 @@
+import 'package:my_cli_tool/my_cli_tool.dart' as my_cli_tool;
+
+void main(List<String> arguments) {
+  print('Hello world: ${my_cli_tool.calculate()}!');
+}
---
+++ test/my_cli_tool_test.dart
@@ -0,0 +1,8 @@
+import 'package:my_cli_tool/my_cli_tool.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
